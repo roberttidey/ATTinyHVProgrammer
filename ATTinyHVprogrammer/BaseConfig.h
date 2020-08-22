a355dfee517c336d71cb3b09fa90af8f1c54bce9
roberttidey <robert"tideys.net>
2020-08-22 22:03:38 +0100

    Use update baseSupport library. Check BaseConfig.h

--- ATTinyHVprogrammer/BaseConfig.h
+++ ATTinyHVprogrammer/BaseConfig.h
@@ -2,6 +2,7 @@
  R. J. Tidey 2019/12/30
  Basic config
 */
+#define FILESYSTYPE 1
  
 /*
 Wifi Manager Web set up
@@ -18,10 +19,13 @@ const char* update_password = "password";
 #define SETUP_START 1
 //define config file name if used 
 //#define CONFIG_FILE "/hvFuseConfig.txt"
-//set to 1 if SPIFFS used
-#define SETUP_SPIFFS 1
+//set to 1 if SPIFFS or LittleFS used
+#define SETUP_FILESYS 1
 //define to set up server and reference any extra handlers required
 #define SETUP_SERVER 1
 //call any extra setup at end
 #define SETUP_END 1
+
+// comment out this define unless using modified WifiManager with fast connect support
+//#define FASTCONNECT true
 #include "BaseSupport.h"
